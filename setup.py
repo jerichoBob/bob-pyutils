b045d0bad4cf1943a5e381f62b6ba1e1fb6368b7
Bob Seaton <robert.w.seaton.jr@gmail.com>
2023-07-05 16:28:52 -0400

    re-ignoring *.egg-info

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     version='0.0.1',
     description='a pip-installable package example',
     license='Apache License 2.0',
-    packages=['bob-pyutils'],
+    # packages=['bob-pyutils'],
     author='Robert Seaton',
     author_email='robert.w.seaton.jr@gmail.com',
-    keywords=['python', 'utilities'],
-    url='https://github.com/bgse/hellostackoverflow'
+    keywords=['python', 'utilities']
 )
